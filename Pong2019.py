adb291ff45c9bf904c717263cc129202ab2597b6
Fred <charruyerfred@gmail.com>
2019-01-06 18:01:54 +0100

    Balle
    creation et positionnement de la balle au centre du terrain de jeu Pong

--- Pong2019.py
+++ Pong2019.py
@@ -7,11 +7,11 @@ fenetre = Tk()
 canevas = Canvas(fenetre, bg = 'black', height = 500, width = 725)
 label = Label(fenetre, text = 'PONG', fg = 'blue', bg = 'black')
 canevas.create_line(362.5,0,362.5,500, fill = 'white', width = 5)
-Bouton_Quitter=Button(fenetre, text ='Quitter', command = fenetre.destroy
+Bouton_Quitter=Button(fenetre, text ='Quitter', command = fenetre.destroy)
 
 # creation de la balle au centre dans notre fenetre et deplacement
 
-Pos_X = 352
+Pos_X = 35
 Pos_Y = 240
 dx = 0
 dy = 5
